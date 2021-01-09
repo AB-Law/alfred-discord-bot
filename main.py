2aaa150165a62ee02c3261ea24233937209e0ff8
Akshay B <akshay.law35@gmail.com>
2021-01-09 15:50:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+import os
+
+client = discord.Client()
+
+token = ''
+name = 'Alfred'
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    sender = str(message.author.id)
+    mention = '<@' + sender + '>'
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('?hello'):
+        await message.channel.send('Hello ' + mention + '! I am', name)
+    if message.content.startswith('?info'):
+
+        await message.channel.send('Made by A Law#7777 (<@186870407738032129>)')
+
+
+client.run()
