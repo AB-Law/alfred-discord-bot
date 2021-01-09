fa82075828429f12f1f308f9d668c6ab131a444c
Akshay B <akshay.law35@gmail.com>
2021-01-09 23:37:33 +0530

    changed from client to bot and added pinging function and others

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
 import discord
 import os
+from discord.ext.commands import Bot
+from discord.ext.commands import has_permissions
 
-client = discord.Client()
-
-token = ''  # Enter your token id here
-name = 'Alfred'  # Name of the bot
+bot = Bot(command_prefix='?')
+TOKEN = ''  # Enter your bot token here
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="At your service, Master A Law#7777"))
 
 
-@client.event
-async def on_message(message):
-    sender = str(message.author.id)
-    mention = '<@' + sender + '>'
+@bot.command(name='server')
+async def fetchServerInfo(context):
+    guild = context.guild
 
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('?hello'):
-        await message.channel.send('Hello ' + mention + '! I am', name)
-    if message.content.startswith('?info'):
-
-        await message.channel.send('Made by A Law#7777 (<@186870407738032129>)')
+    await context.send(f'Server Name: {guild.name}')
+    await context.send(f'Server Size: {len(guild.members)}')
+    await context.send(f'Server Name: {guild.owner.display_name}')
 
 
-client.run()
+@bot.command(name='hello')
+async def hello(message):
+    await message.channel.send('Hello ' + message.author.mention + '! I am Alfred')
+
+
+@bot.command(name='purge', pass_context=True)
+@has_permissions(administrator=True)
+async def clean(ctx, limit: int):
+    limit = limit+1
+    await ctx.channel.purge(limit=limit)
+    await ctx.send('Cleared by {}'.format(ctx.author.mention))
+
+
+@bot.command(name='mention')
+async def args(ctx, arg1=None, arg2=None):
+    try:
+        if arg2 == None:
+            arg2 = 1
+        if arg1 == None:
+            arg1 = ctx.author.mention
+        if arg1 == ctx.guild.owner and ctx.author.mention != ctx.guild.owner:
+            for x in range(int(arg2)):
+                await ctx.send(ctx.author.mention)
+        else:
+            if int(arg2) > 5 and ctx.author.mention != ctx.guild.owner:
+                arg2 = 10
+                for x in range(int(arg2)):
+                    await ctx.send(ctx.author.mention)
+            else:
+                for x in range(int(arg2)):
+                    await ctx.send('Pinging {}'.format(arg1))
+    except:
+        print('hi')
+
+
+bot.run(TOKEN)
